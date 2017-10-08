5b0180e8476fa8e7dd19c65bad2cbef6b1594c6c
codakongur <mattisigur@gmail.com>
2017-10-08 02:58:37 +0000

    countries you can and cant goto

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ var visits = 0;
 
 // IP 130.208.244.125
 // use bodyParser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({extended : true, limit: '5mb'}));
 
 
 console.log("ljotur pafi");
@@ -36,16 +36,56 @@ var q = async.queue(function (task, done) {
     request(task.url, function(err, res, body) {
         if (err) return done(err);
         if (res.statusCode != 200) return done(res.statusCode);
-        //console.log(JSON.parse(body).vaccinations);
-        countriesJSON[counter] = {name: countriesJSON[counter].name, vaccinations:JSON.parse(body).vaccinations};
+        var vacc = JSON.parse(body).vaccinations;
+        var vaccNames = '';
+        for(i = 0; i <= vacc.length -1; i++){
+            vaccNames = vaccNames + " " + vacc[i].name;
+        }
+        countriesJSON[counter] = {name: countriesJSON[counter].name, vaccinations: vacc, allVaccNames: vaccNames};
         counter++;
+        console.log(counter);
         done();
     });
 },5);
 
 function compareVaccines (userVaccines) {
-  // { 'userVaccines[]': [ 'Cholera', 'Hepatitis A', 'Malaria' ] }
+    listOfCountires =[]
+    listofCantGo = [];
+    for(var i = 0; i <= countriesJSON.length - 1; i++){
+        //var vaccNames = countriesJSON[1].allVaccNames;
+        var canTravel = [];
+        var vacc = countriesJSON[i].vaccinations;
+        //console.log(vacc);
+        for(var k = 0; k <= vacc.length - 1; k++){
+            var bool = false;
+            for(var j = 0; j <= userVaccines.length - 1; j++){    
+                if(vacc[k].name === userVaccines[j]){
+                    bool = true;
+                    //break;
+                }
+            }
+            canTravel.push(bool);
+        }
+        var yesHeCan = true;
 
+        console.log(canTravel);
+        
+        for(var z = canTravel.length - 1; z >= 0; z--){
+            if(!canTravel[z]){
+                if(yesHeCan) {
+                    listofCantGo.push({name:countriesJSON[i].name, needVacc:[vacc[z].name]});
+                    yesHeCan = false;
+                } else {
+                    var refresh = listofCantGo.pop();
+                    refresh.needVacc.push(vacc[z].name)
+                    listofCantGo.push(refresh);
+                }
+            }
+        }
+        if(yesHeCan === true){ listOfCountires.push(countriesJSON[i].name)} 
+    }
+    console.log(listofCantGo);
+    return {can : listOfCountires, cant: listofCantGo};
 }
 
 request({
@@ -67,21 +107,21 @@ request({
 app.post('/', function(req, res) {
 
   try {
-    //console.log(req.body);
-    console.log(req.params);
+    //console.log(req.params);
     console.log(req.body);
   }
   catch(e) {
     console.error(e);
   }
 
-  res.send(countriesJSON);
+  res.send(compareVaccines(["Hepatitis A", "Hepatitis B", "Tyfoid", "DTP"]));
 });
 
 
 app.get('/', function(req,res){
     console.log("Request!");
-    res.send(countriesJSON);
+    console.log(compareVaccines(["Hepatitis A", "Hepatitis B", "Tyfoid", "DTP"]));
+    res.send({countriesJSON});
 });
 
 app.listen('8282');
